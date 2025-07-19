9022105881f5146a6cbf0b8f43d41cfcad3c4eaa
Spigell <spigelly@gmail.com>
2025-07-19 18:05:42 +0700

    :wrench: test

--- ci-copy-actions.sh
+++ ci-copy-actions.sh
@@ -3,4 +3,4 @@
 set -xe
 
 rsync -a .github/actions-temp/.github/actions/ .github/actions/
-git update-index --assume-unchanged $(find .github/actions -type f)
+git update-index --assume-unchanged $(find .github -type f)

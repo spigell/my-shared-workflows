5e8890982aebc4a3ca716c4409a25184cc5eeb05
Spigell <spigelly@gmail.com>
2025-07-19 19:00:06 +0700

    :wrench: test

--- ci-copy-actions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-COPY="setup-go-env"
-
-rsync -a .github/actions-temp/.github/actions/${COPY} .github/actions/
-git update-index --assume-unchanged .github/actions/${COPY}/action.yaml
